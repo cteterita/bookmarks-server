3d74cf251337aae9e9c68659ae62c38b768929d4
Claire Lesh <cteterita@gmail.com>
2020-09-15 14:22:13 -0700

    added store

---
+++ src/store.js
@@ -0,0 +1,11 @@
+const bookmarks = [
+  {
+    id: 'bc420fa4-122b-4fa2-a36d-a01744d9c776',
+    title: 'Google',
+    url: 'https://www.google.com',
+    description: 'Don\'t be evil',
+    rating: 5,
+  },
+];
+
+module.exports = { bookmarks };
