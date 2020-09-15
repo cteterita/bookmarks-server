f8ae1cf441299efbdc793175cc1e50044e5c72d3
Claire Lesh <cteterita@gmail.com>
2020-09-15 14:13:56 -0700

    added logging

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 
+const logger = require('./logger');
+
 const app = express();
 
 const morganOption = (NODE_ENV === 'production')
