de044bb6cf9a981383cc1155d83248d1653b3e67
Claire Lesh <cteterita@gmail.com>
2020-09-15 14:14:56 -0700

    added naive authorization

--- src/app.js
+++ src/app.js
@@ -17,6 +17,18 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+app.use((req, res, next) => {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  // move to the next middleware
+  return next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
